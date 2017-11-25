d515b1935f082b5628148adba51b5aa0e41b13c1
Ilona Demidenko <demiluri@gmail.com>
2017-11-25 10:34:27 +0100

    Implement stream statuses

---
+++ src/index.js
@@ -0,0 +1,64 @@
+(function () {
+    "use strict";
+
+    var usersElem = document.querySelector('.users');
+    var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+    function getUserData(name) {
+
+        var apiURL = 'https://wind-bow.gomix.me/twitch-api/streams/' + name + '/';
+
+        var userElem = document.createElement('div');
+        usersElem.appendChild(userElem);
+        userElem.classList.add('user');
+
+        var nameElem = document.createElement('div');
+        userElem.appendChild(nameElem);
+        nameElem.classList.add('name');
+
+
+        var statusElem = document.createElement('div');
+        userElem.appendChild(statusElem);
+        statusElem.classList.add('user');
+
+        nameElem.innerText = name;
+
+        const script = document.createElement('script');
+
+        window['streamCallback__' + name] = function (data) {
+            if (!data.stream) {
+                statusElem.innerText = "No stream";
+                userElem.classList.add('offline');
+            } else {
+                console.log(data.stream);
+                var linkElem = document.createElement('a');
+                linkElem.setAttribute('href', data.stream.channel.url);
+                linkElem.innerText = 'Watch Stream ' + data.stream.channel.status;
+                linkElem.classList.add('link');
+                statusElem.appendChild(linkElem);
+            }
+        };
+
+        script.src = apiURL + '?callback=streamCallback__' + name;
+
+        document.getElementsByTagName('body')[0].appendChild(script);
+
+    }
+
+    document.getElementById('online').addEventListener('click', function () {
+        document.querySelector('.users').classList.add('hide-offline');
+        document.querySelector('#all').classList.remove('selected');
+        document.querySelector('#online').classList.add('selected');
+    });
+
+    document.getElementById('all').addEventListener('click', function () {
+        document.querySelector('.users').classList.remove('hide-offline');
+        document.querySelector('#all').classList.add('selected');
+        document.querySelector('#online').classList.remove('selected');
+    });
+
+    streams.forEach(function (stream) {
+        getUserData(stream);
+    });
+
+})();
